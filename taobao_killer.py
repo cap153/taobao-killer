88f69977dd9f1d43af737376b3b88c1403dbc7a0
cap153 <67049883+cap153@users.noreply.github.com>
2024-10-03 17:08:47 +0800

    Update taobao_killer.py

--- taobao_killer.py
+++ taobao_killer.py
@@ -12,7 +12,7 @@ page.get("https://www.taobao.com")
 # 点击购物车
 page.ele('x://*[@id="J_MiniCart"]/div[1]/a/span[2]').click()
 # 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
-page.wait.ele_displayed('x://*[@id="mainHeaderContainer_1"]/div[2]/label/span[1]/input',timeout=60)
+page.wait.ele_displayed('全选',timeout=60)
 # 点击购物车全选按钮
 page.ele('全选').click()
 
